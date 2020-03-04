37e8177afb51dd5b5c9b05bd4a27251988d918b8
Danielle Walraven <dwalraven21@gmail.com>
2020-03-04 17:49:54 -0500

    Added more helpful links.

--- src/components/FAQs.vue
+++ src/components/FAQs.vue
@@ -1,6 +1,9 @@
 <template>
     <main>
+        <!-- This is where you add your script with your json-ld -->
         <script v-html="jsonld" type="application/ld+json" />
+
+        <!-- The rest of the content for your page goes here -->
         <h1>How do I add rich text to a Vue.js project?</h1>
         <a href="https://github.com/dwalraven21/rich-text-vue/blob/master/src/components/FAQs.vue">
             <div class="github">
@@ -11,11 +14,9 @@
                   <img src="@/assets/GitHub-Mark-64px.png" style="width: 64px;" alt="See Source Code on GitHub"/>
                 </div>
             </div>
-
-
         </a>
         <p>To use rich text in a Vue.js sinlge page application, instead of adding the rich text script to the <code>index.html</code> page, you would add the json data to a Vue component. You can do this by returning a variable containing the json data. You can then add a script with <code>type='application/ld+json'</code> to your template section and use <code>v-html</code> to display the json object on the page.</p>
-        <p>Google has a handy test to make sure your page supports rich results: <a href="https://search.google.com/test/rich-results">https://search.google.com/test/rich-results</a></p>
+        <p>You can use <a href="https://developers.google.com/search/docs/data-types/faqpage">Google's developer guides</a> for help formatting your rich text and once you've created your page, be sure to use <a href="https://search.google.com/test/rich-results">Google's Rich Results Test</a> to make sure the rich text is actually being supported on your page.</p>
         <div class="image-box">
             <img src="@/assets/file-struct.png" alt="The file sctructure I used for my Vue App"/>
             <img src="@/assets/script.png" alt="The script section of my component"/>
@@ -29,6 +30,7 @@ export default {
   name: 'FAQs',
   data(){
         return {
+          // I used google's guide for how to format my json-ld: https://developers.google.com/search/docs/data-types/faqpage
             jsonld: {
                 "@context": "https://schema.org",
                 "@type": "FAQPage",
